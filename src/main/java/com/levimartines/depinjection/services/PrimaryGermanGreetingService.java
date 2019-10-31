d7397c06ec5f2c2b5aa618907799da1c4d88311e
levimartines <levi.mferreira@gmail.com>
2019-10-31 00:28:34 -0300

    default Spring Profile

--- src/main/java/com/levimartines/depinjection/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/levimartines/depinjection/services/PrimaryGermanGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile(value = "de")
 @Primary
-@Profile(value = {"en","default"})
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - 1ST ONE HERE BEATCHES";
+        return "Hallo Welt - German Service";
     }
 }
